db7ecca9df8cea1cc801aace3523501163661b71
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2022-10-09 15:18:49 +0300

    add modal

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import { ImageGalItem, ImageGalleryItemImage } from './ImageGalleryItem.styled';
+import Modal from 'components/Modal/Modal';
 
 class ImageGalleryItem extends Component {
+  state = {
+    isModalOpen: false,
+  };
+
+  toggleModal = () => {
+    this.setState(state => ({ isModalOpen: !state.isModalOpen }));
+  };
+
   render() {
     const { webformatURL, largeImageURL } = this.props;
+    const { isModalOpen } = this.state;
     return (
       <ImageGalItem>
-        <ImageGalleryItemImage src={webformatURL} alt="img" />
+        <ImageGalleryItemImage
+          src={webformatURL}
+          alt="img"
+          onClick={this.toggleModal}
+        />
+        {isModalOpen && (
+          <Modal onClose={this.toggleModal}>
+            <img src={largeImageURL} alt="img" />
+          </Modal>
+        )}
       </ImageGalItem>
     );
   }
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import { Overlay, ModalStyled } from './Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleEsc);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleEsc);
+  }
+
+  handleEsc = evt => {
+    if (evt.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+  handleBackDrop = evt => {
+    if (evt.currentTarget === evt.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <Overlay onClick={this.handleBackDrop}>
+        <ModalStyled>{this.props.children}</ModalStyled>
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
+
+export default Modal;
